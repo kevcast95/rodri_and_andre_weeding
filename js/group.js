2506e240611b19d10f6ee0963acf3284c8298e74
kevcast95 <kevcast06@gmail.com>
2021-05-19 15:16:30 -0500

    data added

---
+++ js/group.js
@@ -0,0 +1,7 @@
+window.onload = function () { 
+ let getInvitados = localStorage.getItem('invitado');
+ let invitado = JSON.parse(getInvitados)
+ let groupName = document.getElementById('group_name')
+ groupName.innerHTML = invitado[0].Grupo;
+ console.log("2dond carg", invitado[0]);
+}
---
+++ js/invitados.js
@@ -0,0 +1,566 @@
+
+let invites = [
+ {
+  "Grupo": "Familia De La Ossa González",
+  "CÓDIGO": "1166",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia De La Ossa Rodríguez",
+  "CÓDIGO": "4275",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Luis Alberto Vega",
+  "CÓDIGO": "1294",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Sierra De La Ossa",
+  "CÓDIGO": "3555",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Eliab Álvarez",
+  "CÓDIGO": "1574",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Jesús De La Ossa",
+  "CÓDIGO": "4297",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia Angulo Triana",
+  "CÓDIGO": "9734",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia Angulo Ibarra",
+  "CÓDIGO": "8528",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia Latorre Angulo",
+  "CÓDIGO": "9700",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia Coymat Angulo",
+  "CÓDIGO": "4148",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia Ibarra Flórez",
+  "CÓDIGO": "5549",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Sabogal Ibarra",
+  "CÓDIGO": "2488",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia González Bonett",
+  "CÓDIGO": "3196",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Franklin Arroyo",
+  "CÓDIGO": "1243",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Valeria Olave",
+  "CÓDIGO": "4091",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Joan Forero",
+  "CÓDIGO": "7067",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Amy Ahumada",
+  "CÓDIGO": "1453",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Kevin Castiblanco",
+  "CÓDIGO": "3076",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Daniela Gaviria",
+  "CÓDIGO": "7211",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Daniel Gómez",
+  "CÓDIGO": "9473",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia Romero Mora",
+  "CÓDIGO": "8744",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia Donoso Elles",
+  "CÓDIGO": "9894",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Wulfran Navarro",
+  "CÓDIGO": "9903",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Laura Ponce",
+  "CÓDIGO": "4544",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia Crissien Turbay",
+  "CÓDIGO": "3790",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Mónica Villamil",
+  "CÓDIGO": "8228",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Alex Castro",
+  "CÓDIGO": "6735",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Diana Jiménez",
+  "CÓDIGO": "8817",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia Gómez Pantoja",
+  "CÓDIGO": "4917",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Jessica Mercado",
+  "CÓDIGO": "8063",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Antony Ledolley",
+  "CÓDIGO": "2618",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Florez De La Ossa",
+  "CÓDIGO": "1162",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia González Urbano",
+  "CÓDIGO": "6304",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Enquire Luis Villero",
+  "CÓDIGO": "5891",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Norma Ballestas",
+  "CÓDIGO": "2049",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Triana Barraza",
+  "CÓDIGO": "2165",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Julieth Cepeda",
+  "CÓDIGO": "8645",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Lara Solano",
+  "CÓDIGO": "4401",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Pablo & Fabiola",
+  "CÓDIGO": "3252",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Jonathan Morelos",
+  "CÓDIGO": "3146",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Wilson Bohórquez",
+  "CÓDIGO": "9351",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "Familia González Padilla",
+  "CÓDIGO": "2807",
+  "MODALIDAD": "PRESENCIAL"
+ },
+ {
+  "Grupo": "José & Gaby",
+  "CÓDIGO": "1297",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Rosellys Rizzo",
+  "CÓDIGO": "8906",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Suárez Angulo",
+  "CÓDIGO": "5751",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Gallego Suárez",
+  "CÓDIGO": "9856",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia González Suárez",
+  "CÓDIGO": "1459",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia López Angulo",
+  "CÓDIGO": "6677",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "René Coymat",
+  "CÓDIGO": "3016",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Angulo Navarro",
+  "CÓDIGO": "3735",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia González Angulo",
+  "CÓDIGO": "2156",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Gómez Angulo",
+  "CÓDIGO": "9379",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Pontón Triana",
+  "CÓDIGO": "6020",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Triana Tello",
+  "CÓDIGO": "8393",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Ailyn Moreno",
+  "CÓDIGO": "7312",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Diana Varela",
+  "CÓDIGO": "8735",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Morales Sierra",
+  "CÓDIGO": "9897",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Dada Triana",
+  "CÓDIGO": "6847",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Coronado Storino",
+  "CÓDIGO": "7891",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Franca Monachello",
+  "CÓDIGO": "6219",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Peñaranda Arroyo",
+  "CÓDIGO": "7426",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Dayana Montes",
+  "CÓDIGO": "4268",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia González Zarante",
+  "CÓDIGO": "7289",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Salgado Pérez",
+  "CÓDIGO": "3893",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Osorio González",
+  "CÓDIGO": "2348",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Vázquez González",
+  "CÓDIGO": "9771",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Álvarez González",
+  "CÓDIGO": "7822",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia González Romero",
+  "CÓDIGO": "8128",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Hernández Yépez",
+  "CÓDIGO": "8334",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia De La Ossa Puerta",
+  "CÓDIGO": "4322",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Escobar De La Ossa",
+  "CÓDIGO": "9291",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia De La Ossa De La Ossa",
+  "CÓDIGO": "6623",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia De La Ossa Arroyo",
+  "CÓDIGO": "4274",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia González González",
+  "CÓDIGO": "7768",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Montes Rodríguez",
+  "CÓDIGO": "8960",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Eljagh Sierra",
+  "CÓDIGO": "1672",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Anuar Eljagh",
+  "CÓDIGO": "7501",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "José Alejandro Espinosa",
+  "CÓDIGO": "7039",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Chimá Maestre",
+  "CÓDIGO": "1440",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Callejas Jiménez",
+  "CÓDIGO": "4044",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Olga Cantillo",
+  "CÓDIGO": "7749",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Rivera Pulgar",
+  "CÓDIGO": "7020",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Todsom & Auridis",
+  "CÓDIGO": "1621",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Isabel Llerena",
+  "CÓDIGO": "8762",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Armando Rodríguez",
+  "CÓDIGO": "9133",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Vera Noriega",
+  "CÓDIGO": "5966",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Rendón Comas",
+  "CÓDIGO": "2873",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Stefanía Guzmán",
+  "CÓDIGO": "6755",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "María Beatriz Anillo",
+  "CÓDIGO": "8570",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Andrés Jaimes",
+  "CÓDIGO": "2740",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Juan Pi & Pau",
+  "CÓDIGO": "8698",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Karolay Delgado",
+  "CÓDIGO": "6969",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Pimienta Huertas",
+  "CÓDIGO": "1762",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Natalie Simancas",
+  "CÓDIGO": "8492",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Loui Ahumada",
+  "CÓDIGO": "3075",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Yesid & Sara",
+  "CÓDIGO": "4447",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Juan Sebastián Gómez",
+  "CÓDIGO": "8235",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Karen Gómez",
+  "CÓDIGO": "9233",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Díaz Altamar",
+  "CÓDIGO": "5412",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Nicolás Rodríguez",
+  "CÓDIGO": "3975",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Luis Povea",
+  "CÓDIGO": "8849",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Carlos Lozano",
+  "CÓDIGO": "3569",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Melanie Cepeda",
+  "CÓDIGO": "5294",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Camilo Verbel & Novia",
+  "CÓDIGO": "7614",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Morelo Janson",
+  "CÓDIGO": "6861",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Edson Morelo",
+  "CÓDIGO": "8059",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Familia Muñoz Vergara",
+  "CÓDIGO": "9350",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Santiago Silva",
+  "CÓDIGO": "1367",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Sasky Garcés",
+  "CÓDIGO": "9475",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Itier & Dayi",
+  "CÓDIGO": "4596",
+  "MODALIDAD": "VIRTUAL"
+ },
+ {
+  "Grupo": "Kevin & Ale",
+  "CÓDIGO": "8365",
+  "MODALIDAD": "VIRTUAL"
+ }
+]
+
+function filterInvites() {
+ let codeI = document.getElementById("input_code").value;
+ console.log("codeI", codeI);
+ console.log("invites:",invites.filter(inv=> inv.CÓDIGO === codeI));
+ let invitado = invites.filter(inv=> inv.CÓDIGO === codeI)
+ localStorage.setItem('invitado',invitado[0])
+}
